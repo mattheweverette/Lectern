a1c23ac7cc45fadaf404561325dff72c8fd52897
Matthew Everette <mattheweverette@gmail.com>
2022-01-30 10:42:43 -0500

    Hid secrets

--- Lectern/Lectern/settings.py
+++ Lectern/Lectern/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&radhaipfgo_jmd)#z1t4gl5j*3=1=tvkz^s$&@iq8pbdj#f%@'
+SECRET_KEY = os.path.expandvars(os.environ['DJANGO_SECRET_KEY'])
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
